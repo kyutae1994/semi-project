b0d585ebf10dbe1056f972d75bf368ff6d85c2bc
kyutae <k305059ute@gmail.com>
2021-04-24 23:57:13 +0900

    Revert "vue 랜더링 이해"
    This reverts commit 8d6a9566fd277f6ed7d5b3a266ca43956fecd95c.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [num, setNum] = useState(5);
+  let list = [1, 2, 3];
 
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '장보고' },
-    { id: 4, name: '이순신' },
-  ];
-
-  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!!
-
-  const download = () => {
-    // fetch().then().then()
-    setUsers([...sample, { id: num, name: '조자룡' }]);
-    setNum(num + 1);
-  };
-
-  // 랜더링 시점 = 상태값 변경
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1>
-          {u.id}, {u.name}
-        </h1>
-      ))}
+      <div>
+        {list.map((n) => (
+          <h1>{n}</h1>
+        ))}
+      </div>
     </div>
   );
 }
